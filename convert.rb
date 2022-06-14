cbeb679d68dde0548d07835a0b4c0f192e2628e0
rob cobb <rwcobbjr@gmail.com>
2022-06-14 12:57:20 -0600

    fix leading number stripping for cases where the whole filename is numeric

--- convert.rb
+++ convert.rb
@@ -21,7 +21,7 @@ MD_NEW_TO_TEXT = {}
 
 UUID_PATTERN = /\ ([a-f0-9]{32})/
 lesson_words = ["week", "lesson"]
-LEADING_NUMS_PATTERN = /^(#{lesson_words.join('|')})?[\d\-\.]*/i
+LEADING_NUMS_PATTERN = /^(#{lesson_words.join('|')})?[\d\-]*(?=[a-zA-Z]+)/i
 # change filenames
 def slugify(item)
   item
